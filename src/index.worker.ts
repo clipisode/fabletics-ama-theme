785a3e072e074355c19689209caf03e95f901170
Brian Alvey <github@subs.frankensite.com>
2021-09-30 18:18:25 -0700

    Update index.worker.ts

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -8,28 +8,17 @@ export const siteData: SiteData = {
   customCssKey: "styles.css",
   meta: {
     // values: TOPIC_TITLE
-    title: "TOPIC_TITLE • RushTix",
-    description: "Send a video request (no app needed) for TOPIC_TITLE",
+    title: "TOPIC_TITLE • Fabletics",
+    description: "Send a video reply (no app needed) for TOPIC_TITLE",
   },
-  introScreen: {
-    // values: SOCIAL_NETWORK, BROWSER_NAME, INVITATION_DISPLAY_NAME, EXTRA_TERMS, BRAND_SLUG, TERMS_SLUG, TERMS_NAME, BRAND_NAME
-    recordButtonLabel: "Record a video request",
-    readyDescription: `
-      <h1 class="text-center text-black font-heading font-extrabold p-0 text-2xl">Hang on</h1>
-      <p class="text-left text-gray-700 text-sm py-3">The SOCIAL_NETWORK browser doesn’t support direct camera uploads on Android.</p>
-      <h1 class="text-center text-black font-heading font-extrabold p-0 text-2xl">Good news</h1>
-      <p class="text-left text-gray-700 text-sm py-3">Just tap the three dots menu and choose <span class="whitespace-nowrap">“Open in BROWSER_NAME”</span> to send your request to <span class="whitespace-nowrap">INVITATION_DISPLAY_NAME</span>.</p>
-    `,
-  },
-
   introScreenDesktop: {
     // values: THEME_FILE_ROOT, TOPIC_TITLE, QR_CODE, INVITATION_DISPLAY_NAME, INVITATION_URL
     markup: `
       <div class="flex flex-col items-center justify-between text-center flex-grow ">
         <div class="flex flex-col items-center justify-between text-center">
           <div class="m-2">
-            <a href="https://rushtix.com">
-              <img src="THEME_FILE_ROOT/icon-black-small.png" class="w-12 h-12" />
+            <a href="https://fabletics.com">
+              <img src="THEME_FILE_ROOT/logo-small.png" class="h-12" />
             </a>
           </div>
           <h1 class="font-heading mt-3 mb-6 text-xl md:text-2xl max-h-28 md:max-h-32 overflow-hidden">TOPIC_TITLE</h1>
@@ -37,34 +26,16 @@ export const siteData: SiteData = {
         <div class="m-8">QR_CODE</div>
         <div>
           <p class="m-4 mb-6 text-base">Point your camera at the QR code to open this link on your
-          phone and send <b>INVITATION_DISPLAY_NAME</b> <span class="whitespace-nowrap">a video request.</span></p>
+          phone and send <b>INVITATION_DISPLAY_NAME</b> <span class="whitespace-nowrap">your video question.</span></p>
         </div>
       </div>
     `,
   },
   nameScreen: {
     // values: UPLOAD_PERCENTAGE, INVITATION_DISPLAY_NAME, SOCIAL_NETWORK
-    instructions: `Add your name and hit Save to send <b>INVITATION_DISPLAY_NAME</b> your <span class="whitespace-nowrap">shout out request.</span>`,
-    saveButtonLabel: "Save my request",
     nameScreenHeader: `
       <div class="my-2 flex flex-col items-center">
-        <img src="THEME_FILE_ROOT/icon-black-small.png" class="w-16 h-16" />
-      </div>
-    `,
-  },
-  closedScreen: {
-    // values: THEME_FILE_ROOT, INVITATION_DISPLAY_NAME
-    markup: `
-      <div class="flex flex-grow flex-col items-center justify-between">
-        <div>
-          <p class="mt-12 mb-12 mx-3 text-center">This invitation is no longer accepting <span class="whitespace-nowrap">shout out requests.</span></p>
-          <div class="m-2">
-            <a href="https://rushtix.com">
-              <img src="THEME_FILE_ROOT/logo-small.png" style="width: 250px; height: 33px; margin: 0 auto;" />
-            </a>
-            <p class="mt-12 mb-12 mx-3 text-center">You'll receive your shout out video from <b>INVITATION_DISPLAY_NAME</b> via email when <span class="whitespace-nowrap">it's ready.</span></p>
-          </div>
-        </div>
+        <img src="THEME_FILE_ROOT/logo-small.png" class="h-16" />
       </div>
     `,
   },
@@ -73,12 +44,11 @@ export const siteData: SiteData = {
     markup: `
       <div class="flex flex-grow flex-col items-center justify-between">
         <div>
-          <p class="mt-12 mb-12 mx-3 text-center">Nice work! Your video request for <b>INVITATION_DISPLAY_NAME</b> <span class="whitespace-nowrap">was sent.</span></p>
+          <p class="mt-12 mb-12 mx-3 text-center">Nice work!<br /><br />Your video reply for <b>INVITATION_DISPLAY_NAME</b> <span class="whitespace-nowrap">was sent.</span></p>
           <div class="m-2">
-            <a href="https://rushtix.com">
-              <img src="THEME_FILE_ROOT/-small.png" style="width: 250px; height: 33px; margin: 0 auto;" />
+            <a href="https://swimply.com">
+              <img src="THEME_FILE_ROOT/logo-small.png" style="width: 250px; margin: 0 auto;" />
             </a>
-            <p class="mt-12 mb-12 mx-3 text-center">You'll receive your shout out video from <b>INVITATION_DISPLAY_NAME</b> via email when <span class="whitespace-nowrap">it's ready.</span></p>
           </div>
         </div>
       </div>
