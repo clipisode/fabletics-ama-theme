eb875f6681cf9fedc14cb4dff931f916219fb825
Brian Alvey <github@subs.frankensite.com>
2021-09-30 18:38:38 -0700

    correct image file

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -18,7 +18,7 @@ export const siteData: SiteData = {
         <div class="flex flex-col items-center justify-between text-center">
           <div class="m-2">
             <a href="https://fabletics.com">
-              <img src="THEME_FILE_ROOT/icon-small.png" class="h-12" />
+              <img src="THEME_FILE_ROOT/icon-black-small.png" class="h-12" />
             </a>
           </div>
           <h1 class="font-heading mt-3 mb-6 text-xl md:text-2xl max-h-28 md:max-h-32 overflow-hidden">TOPIC_TITLE</h1>
@@ -35,7 +35,7 @@ export const siteData: SiteData = {
     // values: UPLOAD_PERCENTAGE, INVITATION_DISPLAY_NAME, SOCIAL_NETWORK
     nameScreenHeader: `
       <div class="my-2 flex flex-col items-center">
-        <img src="THEME_FILE_ROOT/logo-small.png" class="h-16" />
+        <img src="THEME_FILE_ROOT/icon-black-small.png" class="h-16" />
       </div>
     `,
   },
@@ -44,9 +44,9 @@ export const siteData: SiteData = {
     markup: `
       <div class="flex flex-grow flex-col items-center justify-between">
         <div>
-          <p class="mt-12 mb-12 mx-3 text-center">Nice work!<br /><br />Your video reply for <b>INVITATION_DISPLAY_NAME</b> <span class="whitespace-nowrap">was sent.</span></p>
+          <p class="mt-12 mb-12 mx-3 text-center">Nice work!<br /><br />Your video question for <b>INVITATION_DISPLAY_NAME</b> <span class="whitespace-nowrap">was sent.</span></p>
           <div class="m-2">
-            <a href="https://swimply.com">
+            <a href="https://fabletics.com">
               <img src="THEME_FILE_ROOT/logo-small.png" style="width: 250px; margin: 0 auto;" />
             </a>
           </div>
